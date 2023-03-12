f1e1613f42aba5089ad629ea090fdcbd419327f1
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-03-12 19:46:26 +0000

    [Add]railsで画像サイズ変更のための設定、Aboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   devise_for :users
+  get "homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
