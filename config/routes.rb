06df4075981a3ef78405a24a089042ecdbdc590e
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-03-14 14:23:17 +0000

    [Finish]headerとfooterのレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
+
   resources :users, only: [:index, :show, :edit, :update]
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 
-  root to: "homes#top"
-  devise_for :users
   get "homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
