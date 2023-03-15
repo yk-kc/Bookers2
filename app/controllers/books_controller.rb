d410560da0fbbc748db6920be5bbe8119f348d2b
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-03-15 08:47:41 +0000

    [Finish]リンクと記述の表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,8 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
-    @introduction = User.select("introduction")
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -13,15 +12,22 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
