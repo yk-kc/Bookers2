91b3814a6ed2ff8281dda101f720a64534035b41
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-04-03 12:42:03 +0000

    [Finish]応用課題8b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,8 @@ class UsersController < ApplicationController
     @book = Book.new
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
-    @the_day_before = @today_book.count / @yesterday_book.count.to_f
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
-    @the_week_before =   @this_week_book.count / @last_week_book.count.to_f
   end
 
   def edit
