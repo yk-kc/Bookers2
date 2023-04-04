646916c7e9289d513ccde043b52aae0a3026b97a
yk-kc <124197100+yk-kc@users.noreply.github.com>
2023-04-04 07:41:06 +0000

    [Finish]応用課題9b

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    get "search", to: "users#search"
+  end
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 
